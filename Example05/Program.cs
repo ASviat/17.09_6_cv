df7a15e914d1667bb7eaebc75dcd08eb818286ca
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-19 00:59:54 +0300

    Init commit

---
+++ Example05/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int num=Convert.ToInt32(Console.Read());
+int res= num % 10;
+
+Console.WriteLine($"Третья цифра трехзначного числа это: {res} ");
+
+//какой-то бред, но так уж в уроке было.
