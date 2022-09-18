ab3dd5464c1d63c5c19493230ed3e16677fe2db4
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-19 00:11:24 +0300

    Example02 Initial commit

---
+++ Example02/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Выполняю пш-пш программу проверки, является ли первое число квадратом второго");
+Console.Write("Введите первое целое число: ");
+int num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Оу даа");
+Console.WriteLine("Введите второе целое число: ");
+int num2=Convert.ToInt32(Console.ReadLine());
+if(num1==num2*num2)
+{
+    Console.WriteLine ("Таки магия свершилась, таки является квадратом второго ");
+}
+else
+{
+    Console.WriteLine("Неа");
+}
