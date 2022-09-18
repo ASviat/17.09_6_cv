31b0b7636bb6c19f77501f55564984dd5789be8a
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-19 00:48:20 +0300

    Example 04 Init

---
+++ Example04/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите целое число, плиз");
+int num=Convert.ToInt32(Console.ReadLine());
+int count=-num;
+while (count<=num)
+{
+    Console.Write($"{count} ");
+    count++;
+}
