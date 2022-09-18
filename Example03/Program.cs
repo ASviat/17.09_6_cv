840024266cf709fc9a1e41614e2f408b0d4d6bc6
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-19 00:31:57 +0300

    Made bz string, not int

--- Example03/Program.cs
+++ Example03/Program.cs
@@ -1,13 +1,13 @@
 ﻿Console.WriteLine("Введите число от 1 до 7, дабы познать, что за день за ним скрыт.");
-int num=Convert.ToInt32(Console.ReadLine());
-if (num==1) Console.WriteLine("Понедельник");
-if (num==2) Console.WriteLine("Вторник");
-if (num==3) Console.WriteLine("Среда");
-if (num==4) Console.WriteLine("Четверг");
-if (num==5) Console.WriteLine("Пятница");
-if (num==6) Console.WriteLine("Суббота");
-if (num==7) Console.WriteLine("Воскресенье");
-if (num!=1 &&num!=2 && num!=3 && num!=4 && num!=5 && num!=6 && num!=7) Console.WriteLine("Ты болен? От 1 до 7.");
+string num = Console.ReadLine();
+if (num=="1") Console.WriteLine("Понедельник");
+if (num=="2") Console.WriteLine("Вторник");
+if (num=="3") Console.WriteLine("Среда");
+if (num=="4") Console.WriteLine("Четверг");
+if (num=="5") Console.WriteLine("Пятница");
+if (num=="6") Console.WriteLine("Суббота");
+if (num=="7") Console.WriteLine("Воскресенье");
+if (num !="1" && num !="2" && num !="3" && num !="4" && num !="5" && num !="6" && num !="7") Console.WriteLine("Ты болен? От 1 до 7.");
 
 
 Console.WriteLine("Гадания на кофейной гуще +375 29 666 66 66");
